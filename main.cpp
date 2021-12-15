f931cb5d13cdad460410693121399c76c6e35aaa
Justin Berkshire <justin@Justins-MacBook-Pro.local>
2021-12-14 20:46:37 -0600

    pico can be ran and debugged, not sure how to read outputs, binary sucks

--- main.cpp
+++ main.cpp
@@ -18,12 +18,15 @@ int main()
 
     gpio_set_dir(LED, GPIO_OUT);
 
+    stdio_init_all();
+
     while (true)
     {
+        printf("deadly virus");
         gpio_put(LED, 1);
-        sleep_ms(1000);
+        sleep_ms(500);
         gpio_put(LED, 0);
-        sleep_ms(1000);
+        sleep_ms(500);
     }
 
     return 0;
